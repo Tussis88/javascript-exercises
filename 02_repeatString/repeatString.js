848f4b7b5af4fbbfdca7229985b91486c13248cc
Tussis88 <andreab88@hotmail.it>
2024-03-24 17:12:00 +0100

    esercizi 2 e 3 fati

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(stringInput, timesRepeat) {
+  let finalString = "";
+  if (timesRepeat < 0) return "ERROR";
+  for (let i = 0; i < timesRepeat; i++) {
+    finalString += stringInput;
+  }
+  return finalString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+  // for(let i = stringToReverse.lenght; i > 0; i--) {
+  //   newString += stringToReverse.at(i);
+  //   console.log(i);
+  // }
+  let stringArray = stringToReverse.split("");
+  let arrayReversed = stringArray.reverse();
+  let newString = arrayReversed.join("");
 
+  return newString;
 };
 
 // Do not edit below this line
