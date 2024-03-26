5185b2a76d489952faf10471d2e03cae80ebddf5
Tussis88 <andreab88@hotmail.it>
2024-03-27 00:08:47 +0100

    fatto esercizio 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  let total = 0;
+  if (typeof start !== "number" || typeof end !== "number") return "ERROR";
+  if (start < 0 || end < 0) return "ERROR";
+  if (start > end) {
+    let temp = start;
+    start = end;
+    end = temp;
+  }
+  for (let i = start; i <= end; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
