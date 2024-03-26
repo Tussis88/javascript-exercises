a30bc6053127a640f39f00d410ed002ca80cfc97
Tussis88 <andreab88@hotmail.it>
2024-03-26 23:29:17 +0100

    fatto esercizio 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  // for (let j = 0; j < array.length; j++) {
+  //   for (let i = 0; i < args.length; i++) {
+  //     if (array[j] === args[i]) {
+  //       array.splice(j, 1);
+  //     }
+  //   }
+  // }
+  const newArray = array.filter((number) => {
+    return !args.includes(number);
+  })
+  return newArray;
 };
 
 // Do not edit below this line
