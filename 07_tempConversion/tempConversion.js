73064e30954c67dc862a2759692518feb6148251
Tussis88 <andreab88@hotmail.it>
2024-03-27 21:54:50 +0100

    fatto esercizio 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let newTemp = Math.round(((temp - 32) * (5 / 9)) * 10) / 10;
+  return newTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let newTemp = Math.round((temp * (9 / 5) + 32) * 10) / 10;
+  return newTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
