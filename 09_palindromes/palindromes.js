fc204b8182d597fc59bde71a9ed4f8479ea3b13d
Tussis88 <andreab88@hotmail.it>
2024-04-14 14:36:36 +0200

    fatto es. 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+  // MY SOLUTION
+  // array = Array.from(sentence.replace(/[ ,.!]/g, "").toLowerCase());
+  // for (let i = 0; i < array.length / 2; i++) {
+  //   if (array.at(i) !== array.at(-(i + 1))) return false;
+  // }
+  // return true;
+  
+  //THE BETTER ONE
+  const sanitizedString = sentence.replace(/[ ,.!]/g, "").toLowerCase();
+  const reversedString = sanitizedString.split("").reverse().join("");
+  return sanitizedString === reversedString;
 };
 
 // Do not edit below this line
