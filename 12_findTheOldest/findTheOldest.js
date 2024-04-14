f3505d36e5067d8bb209ee873e16c44ce7402963
Tussis88 <andreab88@hotmail.it>
2024-04-14 16:20:41 +0200

    fatto es. 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+  const currentYear = new Date().getFullYear();
+  let olderAge = 0;
+  let oldestPerson = {
+    name: "",
+    yearOfBirth: 0,
+    yearOfDeath: 0,
+  };
+  return peopleArray.reduce((oldestPerson, person) => {
+    if (!person.yearOfDeath) person.yearOfDeath = currentYear;
+    const personAge = person.yearOfDeath - person.yearOfBirth;
+    if (personAge > olderAge) {
+      olderAge = personAge;
+      oldestPerson = person;
+    }
+    return oldestPerson;
+  }, oldestPerson);
 };
 
 // Do not edit below this line
